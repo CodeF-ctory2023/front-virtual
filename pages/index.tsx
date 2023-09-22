bf963fbb1a9713f9614038be48e63d80dde54ce2
Miguel Angel <miguel.puerta@udea.edu.co>
2023-09-22 14:17:06 +0900

    Add informacion personal html formulario

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
-import { ExampleComponent } from '@/components/ExampleComponent';
+import { UserCircleIcon } from '@heroicons/react/24/outline'
 
 const Home = () => {
   return (
-    <main className='flex h-screen w-full items-center justify-center'>
-      <ExampleComponent text='Welcome to CodeF@ctory' />
-    </main>
+    <div>
+      <div className="relative z-1">
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="fixed inset-y-0 left-0 flex max-w-full">
+              <div className="pointer-events-auto relative w-screen max-w-[340px]">
+                <div className="flex h-full flex-col bg-white py-6 shadow-xl">
+                  <div className="px-4 sm:px-2">
+                    <button className="h-30 w-32" onClick={() =>  window.location.href='/perfil'}>
+                      <div className="flex place-content-center">
+                        <UserCircleIcon className="h-20 w-20" aria-hidden="true" />
+                      </div>
+                      <div className="text-base text-center font-bold leading-6 text-gray-500">
+                        Perfil
+                      </div>
+                    </button>
+                  </div>
+                  <div className="relative mt-6 flex-1 px-4 sm:px-6"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
