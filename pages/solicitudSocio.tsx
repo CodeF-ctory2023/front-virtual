ef7c1453de2379e7148a800c360c5ffbef238be9
Arboleda5252 <duvan.arboleda1@udea.edu.co>
2023-09-28 12:00:12 -0500

    Se agrego la tabla de solicitud de socio

---
+++ pages/solicitudSocio.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Tabla from '@/components/GestionSociosComponent/table';
+
+const data = [
+  {
+    title1: '####',
+    title2: 'xxxxxx',
+    title3: '####',
+    title4: '',
+    title5: 'P.Judicial',
+    title6: 'ACEPTADO',
+    title7: ''
+  },
+  {
+    title1: '####',
+    title2: '----',
+    title3: '####',
+    title4: '',
+    title5: 'P.Judicial',
+    title6: 'rechazado',
+    title7: ''
+  },
+];
+
+const secondPage: React.FC = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center bg-blue-socio p-2 rounded-sm">
+        <h1 className="text-white text-center font-semibold">
+          Solicitud de Socios
+        </h1>
+      </div>
+      <div className="p-2">
+        <Tabla data={data} />
+      </div>
+    </div>
+  );
+}
+
+export default secondPage;
