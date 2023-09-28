e688cbcd45d9451ba40ba7456654f43f3016ea65
leidycas017 <58440602+leidycas017@users.noreply.github.com>
2023-09-28 10:10:11 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    
+      colors:{
+        
+        'green-socio': '#ABD08D',
+        'peach-socio': '#FBCB9E',
+        'red-socio': '#F39A9E',
+        'gray-socio': '#D6D6D6',
+        'blue-socio': '#6662D9',
+        'lightBlue-socio': '#D7D5EC',
+        'light-socio': '#EFEFF8',
+      }
     },
   },
   plugins: [],
